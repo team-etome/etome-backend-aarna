6d04394f01d2a4b33d29e8b99a4ccdeebd219757
Afif <mk.afifmuhammad@gmail.com>
2023-12-18 12:44:09 +0530

    admin file changed

--- backend/app1/admin.py
+++ backend/app1/admin.py
@@ -6,3 +6,9 @@ from .models import *
 
 
 admin.site.register(God)
+admin.site.register(Admin)
+admin.site.register(Department)
+admin.site.register(Subject)
+admin.site.register(Semester)
+admin.site.register(Teacher)
+admin.site.register(Student)
