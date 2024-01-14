5903c254cb92803060d8ccec734c9d50d5e4fff3
leywin <leywin@duck.com>
2024-01-14 20:32:29 +0530

    fixed conflict

--- backend/flutter/serializers.py
+++ backend/flutter/serializers.py
@@ -1,13 +1,11 @@
-# from rest_framework import serializers
-# from flutter.models import *
+from rest_framework import serializers
+from flutter.models import *
 
 
 
 # class DataSerializer(serializers.ModelSerializer):
 
 
-    class Meta:
-        model = Byte
-        fields = ['image_name','data']
-
-    
+class Meta:
+    model = Byte
+    fields = ['image_name','data']
